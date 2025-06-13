fec225ee7e58add595757b8ded379dbef31cc913
Farida <elibeyovaferide25@gmail.com>
2025-06-13 14:44:32 +0400

    Initial Commit

---
+++ LearnSwiftUi(Concurrency)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnSwiftUi(Concurrency)
+//
+//  Created by Farida on 13.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnSwiftUi(Concurrency)/LearnSwiftUi_Concurrency_App.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUi_Concurrency_App.swift
+//  LearnSwiftUi(Concurrency)
+//
+//  Created by Farida on 13.06.25.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUi_Concurrency_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
